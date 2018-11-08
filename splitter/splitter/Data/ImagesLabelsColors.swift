9be3e49854b98fba0add0f1d4a36f70f4af3f7f6
William Bodily <william.bodily@gmail.com>
2018-11-07 22:48:20 -0500

    Added data file to populate the app data

---
+++ splitter/splitter/Data/ImagesLabelsColors.swift
@@ -0,0 +1,18 @@
+//
+//  ImagesLabelsColors.swift
+//  splitter
+//
+//  Created by William Bodily on 11/7/18.
+//  Copyright © 2018 William Bodily. All rights reserved.
+//
+
+import UIKit
+
+let camera = (image: UIImage(named: "1"), label: "CAMERA", color: #colorLiteral(red: 0.9994266629, green: 0.1180744097, blue: 0.001577521442, alpha: 1))
+let city = (image: UIImage(named: "2"), label: "CITY", color: #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1))
+let animals = (image: UIImage(named: "3"), label: "ANIMALS", color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+let flowers = (image: UIImage(named: "4"), label: "FLOWERS", color: #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1))
+let stand = (image: UIImage(named: "5"), label: "STAND", color: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))
+let urban = (image: UIImage(named: "6"), label: "URBAN", color: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1))
+
+let imagesLabelsColors = [camera, city, animals, flowers, stand, urban]
